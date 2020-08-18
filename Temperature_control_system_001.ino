513a3118252242963bcb6b2f33ab4d3c5f878c77
Hanna <hanna.paivasto@gmail.com>
2020-08-18 21:13:42 +0300

    adding libraries

--- Temperature_control_system_001.ino
+++ Temperature_control_system_001.ino
@@ -1,5 +1,32 @@
 
-// Temperature_control_system_001
+/* Temperature_control_system_001 C Hanna P */
+/********************************************/
+
+/*-----( Import needed libraries )-----*/
+#include <Wire.h>  // Comes with Arduino IDE
+// Get the LCD I2C Library here:
+// https://bitbucket.org/fmalpartida/new-liquidcrystal/downloads
+// Move any other LCD libraries to another folder or delete them
+// See Library "Docs" folder for possible commands etc.
+
+#include <LiquidCrystal_I2C.h>
+
+/*************************************************** 
+  This is a library for the Adafruit PT100/P1000 RTD Sensor w/MAX31865
+  Designed specifically to work with the Adafruit RTD Sensor
+  ----> https://www.adafruit.com/products/3328
+  This sensor uses SPI to communicate, 4 pins are required to  
+  interface
+  Adafruit invests time and resources providing this open source code, 
+  please support Adafruit and open-source hardware by purchasing 
+  products from Adafruit!
+  Written by Limor Fried/Ladyada for Adafruit Industries.  
+  BSD license, all text above must be included in any redistribution
+ ****************************************************/
+
+#include <Adafruit_MAX31865.h>
+
+/*-----( Declare Constants )-----*/
 
 void setup() {
   // put your setup code here, to run once:
